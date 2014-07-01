e9d698236d80fa1acc7ee77a194ccce2d9caaea4
Steve Trease <stevetrease@gmail.com>
2014-07-01 17:56:16 +0100

    Initial Commit

---
+++ Swift Push/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Swift Push
+//
+//  Created by steve on 01/07/2014.
+//  Copyright (c) 2014 Trease. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
