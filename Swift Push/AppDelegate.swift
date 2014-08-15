0f533bedabcd9642a83b6f064c54c3971fa2ae00
Steve Trease <stevetrease@gmail.com>
2014-08-15 11:20:52 +0100

    Added screenshot detection.

--- Swift Push/AppDelegate.swift
+++ Swift Push/AppDelegate.swift
@@ -8,15 +8,14 @@
 
 import UIKit
 import Foundation
+import CoreData
 
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
                             
     var window: UIWindow?
-    
     var tableView: UITableView!
-    
     
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: NSDictionary?) -> Bool {
         // Override point for customization after application launch.
@@ -40,7 +39,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         
         application.registerUserNotificationSettings( settings )
         application.registerForRemoteNotifications()
-
+        
+        // screenshot notification
+        let mainQueue = NSOperationQueue.mainQueue()
+        NSNotificationCenter.defaultCenter().addObserverForName(UIApplicationUserDidTakeScreenshotNotification, object: nil, queue: mainQueue) { notification in
+            println("screenshot taken")
+        }
+            
         return true
     }
 
@@ -58,10 +63,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         println()
 
     }
+    
+    
     func application(application: UIApplication!, didFailToRegisterForRemoteNotificationsWithError error:NSError!) {
         println("Failed to recieve device token")
         println( error.localizedDescription )
     }
+    
     
     func application(application: UIApplication!, didReceiveRemoteNotification userInfo:NSDictionary!, fetchCompletionHandler completionHandler: ((UIBackgroundFetchResult) -> Void)!) {
         println ("Push message received by AppDeligate: \(userInfo)")
@@ -74,6 +82,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         var item = NotificationData()
         item.alert = alert
         item.payload = payload
+        
+
+        // let notificationEntity = NSEntityDescription.entityForName("Notifications", inManagedObjectContext: managedContext)
+        // let newNotification = Notifications(entity: notificationEntity, insertIntoManagedObjectContext: managedContext)
+        
+        // newNotification.alert = alert
+        // newNotification.payload = payload
         
         // add the idem to the data list
         notifications.insert(item, atIndex: 0)
@@ -96,13 +111,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     }
 
-
-
+ 
     
     func applicationWillResignActive(application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
     }
+    
 
     func applicationDidEnterBackground(application: UIApplication) {
         // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
@@ -114,7 +129,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
         println("applicationWillEnterForeground")
     }
-
+    
     func applicationDidBecomeActive(application: UIApplication) {
         // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
         println("applicationDidBecomeActive")
